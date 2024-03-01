03beea786e98a04eb18687976f31b1b1ca56e96d
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-03-02 04:35:55 +0600

    Add search functionality with show all btn

--- js/phone.js
+++ js/phone.js
@@ -1,12 +1,26 @@
-const loadPhone=async ()=>{
-    const res=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone=async (phoneName)=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`);
     const data=await res.json();
     const phonesDetails=data.data
+    // console.log(phonesDetails);
     displayPhone(phonesDetails);
 }
 const displayPhone=phonesDetails=>{
-    const phoneContainer=document.getElementById('card-container')
-    phonesDetails.forEach(phone => {
+    const phoneContainer=document.getElementById('card-container');
+    // clear phone container for new search result 
+    phoneContainer.textContent='';
+
+    const showAllBtnContainer=document.getElementById('show-all-btn-container');
+    if(phonesDetails.length>12){
+        showAllBtnContainer.classList.remove('hidden');
+    }
+    else{
+        showAllBtnContainer.classList.add('hidden')
+    }
+
+    const phones=phonesDetails.slice(0,12);
+
+    phones.forEach(phone => {
         console.log(phone);
         const phoneCard=document.createElement('div');
         phoneCard.classList=`card bg-base-100 shadow-2xl`;
@@ -23,4 +37,11 @@ const displayPhone=phonesDetails=>{
         phoneContainer.appendChild(phoneCard);
     });
 }
+
+const handleSearch=()=>{
+    const inputField=document.getElementById('input-field');
+    const searchName=inputField.value;
+    loadPhone(searchName);
+}
+
 loadPhone();    
